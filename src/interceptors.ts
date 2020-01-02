d7d0b39b9bd20713fce37e6c2191937fecf60a0e
tao.xu <tao.xu>
2020-01-02 14:56:57 +0800

    [feature][单点登陆拦截cas认证]

--- src/interceptors.ts
+++ src/interceptors.ts
@@ -14,53 +14,53 @@ function hasPermission(roles, permissionRoles) {
 }
 router.beforeEach(async(to, from, next) => {
     NProgress.start();
-    if (getToken()) {
-        console.log(to)
-        if (to.path === '/login') {
-            next({
-                path: '/'
-            });
-        } else {
-            // 每次刷新会重新获取菜单ID、校验权限
-            if (store.getters.addRouterLenght == 0) {
-                try{ 
-                    await store.dispatch('handleMenusShow');
-                    router.addRoutes(store.getters.addRouters);
-                    next(to.fullPath);
-                }catch(err){
-                    console.log(err)
+    if(to.path == "/cas"){
+        try{ 
+            const res = await store.dispatch("getRunning")
+            if(res.status == 0){
+                if(to.query.type == "1"){
+                    next({
+                        path:'/inforManage/contractFillList',
+                        query:{companyName:to.query.companyName}
+                    })
                 }
-            } else {
-                next();
             }
-        }
-    } else {
-        if (whiteList.indexOf(to.path) !== -1) {
-            next();
-        } else {
-            if(to.path == "/cas"){
-                try{ 
-                    const res = await store.dispatch("getRunning")
-                    if(res.status == 0){
-                        if(to.query.type == "1"){
-                            next({
-                                path:'/inforManage/contractFillList',
-                                query:{companyName:to.query.companyName}
-                            })
-                        }
+        } 
+        catch(error){ 
+            next('/login');
+            NProgress.done();
+        }      
+    }else{
+        if (getToken()) {
+            console.log(to)
+            if (to.path === '/login') {
+                next({
+                    path: '/'
+                });
+            } else {
+                // 每次刷新会重新获取菜单ID、校验权限
+                if (store.getters.addRouterLenght == 0) {
+                    try{ 
+                        await store.dispatch('handleMenusShow');
+                        router.addRoutes(store.getters.addRouters);
+                        next(to.fullPath);
+                    }catch(err){
+                        console.log(err)
                     }
-                } 
-                catch(error){ 
-                    next('/login');
-                    NProgress.done();
-                }      
-            }else{
+                } else {
+                    next();
+                }
+            }
+        } else {
+            if (whiteList.indexOf(to.path) !== -1) {
+                next();
+            } else {
                 next('/login');
                 NProgress.done();
             }
-            
         }
     }
+    
 });
 
 router.afterEach(() => {
