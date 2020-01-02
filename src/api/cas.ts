3821ba6ac9ef1ff12c6c57f0503e68f5d9c7f544
tao.xu <tao.xu>
2020-01-02 16:41:47 +0800

    [feature][cas接口url写死]

--- src/api/cas.ts
+++ src/api/cas.ts
@@ -3,7 +3,8 @@ import axios from 'axios';
 export function getRunningLogin() {
     return new Promise((resolve, reject)=>{
         axios({
-            url:"./OA/cas/auth",
+            // url:"./OA/cas/auth",//正式
+            url:"../OA/cas/auth",//测试
             method:"get",
         }).then(res=>{
             resolve(res)
