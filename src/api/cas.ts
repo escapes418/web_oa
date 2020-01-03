57d365f322480365ad00bcf49cd22c69d2d5190b
tao.xu <tao.xu>
2020-01-03 16:54:04 +0800

    [feature][cas接口修改名字]

--- src/api/cas.ts
+++ src/api/cas.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 export function getRunningLogin() {
     return new Promise((resolve, reject)=>{
         axios({
-            // url:"./OA/cas/auth",//正式
-            url:"../OA/cas/auth",//测试
+            // url:"./OA/session/auth",//正式
+            url:"../OA/session/auth",//测试
             method:"get",
         }).then(res=>{
             resolve(res)
