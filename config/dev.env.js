00f37299c87eccc877b869c032bee1b32b6def22
haoyuan.she <shehy@sijibao.com>
2020-04-16 22:14:02 +0800

    1

--- config/dev.env.js
+++ config/dev.env.js
@@ -12,7 +12,7 @@ module.exports = merge(prodEnv, {
     // BASE_API: '"http://192.168.12.37:8080/api"',// 张江浩 IP
     // BASE_APIR: '"http://192.168.12.79:8080"',//oa_黄凯
     // BASE_API:'"http://192.168.12.134:8081/api"',
-    BASE_API:'"http://192.168.12.134:8087/api"',
+    BASE_API:'"http://192.168.6.17:8087/api"',
     // BASE_API: '"http://oa.sijibao.co/api"', 
     // BASE_API:'"http://192.168.6.16:8080/api"',
     // BASE_API:'"http://192.168.12.112:8087/api"',
--- config/index.js
+++ config/index.js
@@ -39,7 +39,7 @@ module.exports = {
         // },
 
         // Various Dev Server settings
-        host: "192.168.36.102", // can be overwritten by process.env.HOST
+        host: "192.168.1.10", // can be overwritten by process.env.HOST
         // host: "192.168.12.97", // can be overwritten by process.env.HOST
         port: 8888, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
         autoOpenBrowser: true,
