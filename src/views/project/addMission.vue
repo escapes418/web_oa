d41410a35e61484c7d0a1fa00e51838cdbb18129
tao.xu <tao.xu>
2020-05-14 15:39:22 +0800

    [feature][任务名称长度校验调整]

--- src/views/project/addMission.vue
+++ src/views/project/addMission.vue
@@ -11,7 +11,7 @@
                             <span class="right-con">{{$route.query.parentTaskCode || ""}}</span>
                         </RedStar>
                         <RedStar label="任务名称：" :required="true">
-                            <el-input type="text" placeholder="请输入" style="width:250px;" v-model.trim="postData.taskName" :maxlength="32"></el-input>
+                            <el-input type="text" placeholder="请输入" style="width:250px;" v-model.trim="postData.taskName" :maxlength="64"></el-input>
                         </RedStar>
                         <RedStar label="任务负责人：" :required="true">
                             <el-select clearable class="filter-item" v-model="postData.principalId" placeholder="请选择" style="width:250px;">
